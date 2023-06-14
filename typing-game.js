576c1bfc7a1fd79c63c53be5483bc7dfbcf4f538
Nico <odelotsalocin@gmail.com>
2023-06-14 14:25:55 -0300

    game basics working

--- typing-game.js
+++ typing-game.js
@@ -1,12 +1,13 @@
 const d = document;
-//const words = await getWords();
-//console.log(words.jsonWords);
 const WordContainerDOM = d.querySelector('.word');
-
-
+let word = null;//global scope
+let wordIndex = 0;
+let wordLettersDOM = null;
+let poinstCounter = d.querySelector('p.counter');
+let points = 0;
+let words = null;
 d.addEventListener("DOMContentLoaded", function(){
-    console.log("hola");
-    startGame();
+    setGame();
 })
 
 function getWords(){
@@ -18,32 +19,60 @@ function getWords(){
     ))
 }
 
-async function getData(){
-    let data = await getWords()
-    console.log(data);
-}
-
-
 function showWord(word){
     /* This function puts a word in the WordContainerDOM */
     for(let i = 0; i < word.length; i++){
-        // console.log(word[i])
-        let letter = document.createElement('p');
+        let letter = d.createElement('p');
         letter.innerHTML = word[i];
         letter.classList.add('wletter');
         WordContainerDOM.appendChild(letter);
     }
 }
 
-async function startGame(){
-    const words = await getWords();
-    let word = getRandomWord(words.jsonWords);
-    console.log("random word: " + word)
+async function setGame(){
+    poinstCounter.innerHTML = 0;
+    words = await getWords();
+    word = getRandomWord(words.jsonWords);
     showWord(word);
+    wordLettersDOM = d.querySelectorAll('p.wletter')
+}
+
+function clearWord(){
+    let letras = d.querySelectorAll('p.wletter');
+        letras.forEach(element => {
+        element.remove();
+    });
+}
+
+function restartGame(){
+    points = 0;
+    poinstCounter.innerHTML = points;
+    clearWord();
+    wordIndex = 0;
+    word = getRandomWord(words.jsonWords);
+    showWord(word);
+    wordLettersDOM = d.querySelectorAll('p.wletter')
 }
 
 d.addEventListener("keydown", (event) =>{
-  console.log("word")
+    if(wordIndex == word.length-1){//si se cumple es porque ya se completo la palabra
+        points++;
+        poinstCounter.innerHTML = points;
+        wordLettersDOM[wordIndex].classList.add('wletter-typed');
+        word = getRandomWord(words.jsonWords);
+        clearWord();
+        showWord(word);
+        wordIndex = 0;
+        wordLettersDOM = d.querySelectorAll('p.wletter')
+    }else{
+        if(event.key === word[wordIndex]){//if you tipe well
+            wordLettersDOM[wordIndex].classList.add('wletter-typed');
+            console.log(wordLettersDOM[wordIndex]);
+            wordIndex++;
+        }else{//if you fail typing
+            restartGame();
+        }
+    }
 });
 
 function getRandomWord(words){
@@ -60,7 +89,3 @@ function randomElement(array){
     return array[getRandomInt(array.length)];
 }
 
-
-// function startGame(words){
-//     let randomWord = randomElement(words);
-// }
